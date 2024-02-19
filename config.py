c722e67cb40646f08e1815dc42a6d18189521637
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-02-19 19:10:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "3e74a9830493e9261410a947428dbb34"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "6322247029"
-SUPPORT_GRP = "https://t.me/vip_chattings"
+SUPPORT_GRP = ""
 UPDATE_CHNL = "https://t.me/+3qCjioLlvxZhZDk1"
 OWNER_USERNAME = "@SUBHI_LOVE"
 
